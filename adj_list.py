a919edcaed0b1a1cf7c0365762eb54aeb6577363
Matthew Bolton <matthewwbolton@gmail.com>
2020-08-11 12:29:37 -0400

    -completed all methods for Graph class in adj_list.py

--- adj_list.py
+++ adj_list.py
@@ -47,10 +47,100 @@ class Graph:
                     q.enqueue(next_vertex)
 
     def dft(self, starting_vertex_id):
-        pass
+        # Create an empty stack and push starting_vertex
+        s = Stack()
+        s.push(starting_vertex_id)
+
+        # Create a set to store the visited vertices
+        visited = set()
+
+        # While the stack is not empty
+        while s.size > 0:
+            # pop the last vertex
+            v = s.pop()
+
+            # if vertex has not been visited
+            if v not in visited:
+                # mark the vertex as visited
+                visited.add(v)
+                # print it for debug
+                print(v)
+
+                # add all of its neighbors to the top of the stack
+                for next_vertex in self.get_neighbors(v):
+                    s.push(next_vertex)
 
     def bfs(self, starting_vertex_id, target_vertex_id):
-        pass
+        # create an empty queue and enqueue PATH to the starting_vertex_id
+
+        q = Queue()
+
+        q.enqueue([starting_vertex_id])
+
+        # create a set to store the visited vertices
+
+        visited = set()
+
+        # while the queue is not empty:
+        while q.size() > 0:
+            # dequeue the first PATH
+            path = q.dequeue()
+            # grab the last vertex from the PATH
+            last_vertex = path[-1]
+
+            # check if the vertex has not been visited
+            if last_vertex not in visited:
+                # is it the target_vertex??
+                if last_vertex == target_vertex_id:
+                    # return the PATH
+                    return path
+                # mark the node as visited
+                visited.add(last_vertex)
+
+            # then add a PATH to neighbors to the back of the queue
+
+            neighbors = self.get_neighbors(last_vertex)
+
+            for neighbor in neighbors:
+                # make a copy of the PATH
+                new_path = list(path)
+
+                # append the neighbor to the back of the PATH
+                new_path.append(neighbor)
+                # enqueue our new PATH
+                q.enqueue(new_path)
+
+        # return None
+        return None
 
     def dfs(self, starting_vertex_id, target_vertex_id):
-        pass
+
+        s = Stack()
+
+        visited = set()
+
+        s.push([starting_vertex_id])
+
+        while s.size() > 0:
+
+            path = s.pop()
+
+            last_vertex = path[-1]
+
+            if last_vertex not in visited:
+
+                if last_vertex == target_vertex_id:
+                    return path
+
+                visited.add(path)
+
+            neighbors = self.vertices[last_vertex]
+
+            for neighbor in neighbors:
+                new_path = [path]
+
+                new_path.append(neighbor)
+
+                s.push(new_path)
+
+        return None
